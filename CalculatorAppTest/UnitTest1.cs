55f1eab64f1d598617ae1de8662a7b183aa8e701
Dr. Laszlo Blazovics <blazovics.laszlo@aut.bme.hu>
2024-04-30 10:33:51 +0200

    Tests in

---
+++ CalculatorAppTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorApp.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+    [TestMethod]
+    public void AddTest()
+    {
+
+        int? retVal = Calculator.calculate(2, 3, Operation.Add);
+
+        Assert.AreEqual(5, retVal);
+
+    }
+}
